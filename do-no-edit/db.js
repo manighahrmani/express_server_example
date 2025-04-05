89cdd2faad3c486c0957b3dc92a1308ef9a7ed24
manighahrmani <mashapicasso@gmail.com>
2025-04-05 21:08:03 +0100

    🆕 Add db.js for database client setup and SQL file reading functionality

--- do-no-edit/db.js
+++ do-no-edit/db.js
@@ -2,20 +2,16 @@ import pkg from 'pg';
 import { readFile } from 'fs/promises';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import dbConfig from '../db-config.json';
+
 export const { Client } = pkg;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export const client = new Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: '', // Add your password for the PostgreSQL user here
-  port: 5432,
-});
+export const client = new Client(dbConfig);
 
 export async function readSqlFile(filename) {
-  const path = join(__dirname, 'db', filename);
+  const path = join(__dirname, '../db', filename);
   return await readFile(path, 'utf8');
 }
